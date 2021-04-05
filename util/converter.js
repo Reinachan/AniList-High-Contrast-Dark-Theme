bcbc49ac64e15b63282677910d0f8520fe515fc4
Reina <peri231231@gmail.com>
2021-04-05 19:22:30 +0200

    Fix auto-conversion to mobile bookmarklet

--- util/converter.js
+++ util/converter.js
@@ -1,15 +1,13 @@
-var http = require('http');
-const core = require('@actions/core');
 const fs = require('fs');
 
-const themeCss = fs.readFileSync('High-Contrast-Dark-Theme.user.css', 'utf8');
+const themeLess = fs.readFileSync('gen/theme.css', 'utf8');
 
 const styleString = `
 function addStyleString(str) {
   const node = document.createElement('style');
   node.innerHTML = str;
 
-  document.body.appendChild(node)
+  document.body.appendChild(node);
 }
 
 addStyleString(
@@ -19,6 +17,14 @@ const functionEnd = ');';
 
 const convertToJs = (file) => {
 	let converted = file;
+
+	// Remove stuff not needed on mobile to make it shorter
+	converted = converted.replace(
+		/\/\* no-mobile-start \*\/[\s\S]+?\/\* no-mobile-end \*\//g,
+		''
+	);
+	converted = converted + ':root {font-size:12px;}';
+
 	// Remove comments
 	converted = converted.replace(/\/\*(.|n)*?\*\//gs, '');
 	converted = converted.replace(/(?<!:)\/\/.+/g, '');
@@ -35,38 +41,31 @@ const convertToJs = (file) => {
 	// Convert " to '
 	converted = converted.replace(/"/g, "'");
 
-	return converted;
+	const start = styleString.replace(/\n/g, ' ') + '`';
+	return start + converted + '`' + functionEnd;
 };
-/* eslint-disable */
-const bookmarkletStart = 'javascript:(function () {';
-/* eslint-enable */
 
-const bookmarkletEnd = '})()';
+const bookmarkletStart = 'javascript:(function () {';
+
+const bookmarkletEnd = '})();';
 
 const bookmarlet = (jsScript) => {
-	// Replace all spaces and newlines with a space
 	let converted = jsScript;
+
+	// Replace all spaces and newlines with a space
 	converted = converted.replace(/[\s]+/g, ' ');
 
-	return bookmarkletStart + '"' + converted + '"' + bookmarkletEnd;
+	// remove all spaces after { and before }
+	converted = converted.replace(/{ /g, '{');
+	converted = converted.replace(/ }/g, '}');
+	// remove spaces after colon and semicolon
+	converted = converted.replace(/(:|;|,) /g, '$1');
+
+	return bookmarkletStart + converted + bookmarkletEnd;
 };
-
-// console.log(styleString + convertToJs(themeCss) + functionEnd);
-
-const outputFile =
-	styleString + '`' + convertToJs(themeCss) + '`' + functionEnd;
-
-fs.writeFileSync('High-Contrast-Dark-Theme.js', outputFile, 'utf8');
 
 fs.writeFileSync(
 	'High-Contrast-Dark-Theme.bookmarklet.js',
-	bookmarlet(convertToJs(themeCss)),
+	bookmarlet(convertToJs(themeLess)),
 	'utf8'
 );
-
-// fs.writeFileSync('High-Contrast-Dark-Theme.js', outputFile, 'utf8');
-
-/* fs.writeFile('High-Contrast-Dark-Theme.js', themeCss, (err) => {
-	if (err) throw err;
-	console.log('completed');
-}); */
