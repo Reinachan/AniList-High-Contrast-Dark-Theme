424ed6d000d6004d7c49a14cf9bf789b269ee826
Reina <peri231231@gmail.com>
2022-01-21 14:09:21 +0100

    Finished accidentally unfinished converter.js Added a trigger to re-run builds on changes in converter.js

--- util/converter.js
+++ util/converter.js
@@ -15,27 +15,8 @@ const convertToUserscriptExtension = (css, meta) => {
 	return meta + removeMozDocument(css);
 };
 
-const bookmarkletStart = 'javascript:(function () {';
-
-const bookmarkletEnd = '})();';
-
-const bookmarlet = (jsScript) => {
-	let converted = jsScript;
-
-	// Replace all spaces and newlines with a space
-	converted = converted.replace(/[\s]+/g, ' ');
-
-	// remove all spaces after { and before }
-	converted = converted.replace(/{ /g, '{');
-	converted = converted.replace(/ }/g, '}');
-	// remove spaces after colon and semicolon
-	converted = converted.replace(/(:|;|,) /g, '$1');
-
-	return bookmarkletStart + converted + bookmarkletEnd;
-};
-
 fs.writeFileSync(
 	'High-Contrast-Dark-Theme-Userscript.user.css',
-	bookmarlet(convertToUserscriptExtension(themeCSS, metadata)),
+	convertToUserscriptExtension(themeCSS, metadata),
 	'utf8'
 );
